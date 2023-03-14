874814a3e9eb009f47cc4100ff37eeb1563580b3
RichardOkello2 <okellor2@gmail.com>
2023-03-14 05:14:46 -0700

    Js tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const el of arr) {
   console.log(el);
 }
